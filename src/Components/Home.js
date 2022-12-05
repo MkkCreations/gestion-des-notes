b06801ab83c9080ebe8c7f95d310e2ab7baa9448
KhaffouKhaffou Mohamed <mohamed.khaffou-khaffou4@etu.univ-lorraine.fr>
2022-12-05 13:29:52 +0100

    Fin

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -16,28 +16,28 @@ function Home() {
   }
 
   const Ressources = [
-    {id:3.01, subj: "Initiation au développement", note: [12,24]},
-    {id:3.02, subj: "Développement d'interfaces web", note: [34]},
-    {id:3.03, subj: "Développement orienté objets", note: [34]},
-    {id:3.04, subj: "Développement d'applications avec IHM", note: [34]},
-    {id:3.05, subj: "Qualité de developpement", note: [34]},
-    {id:3.06, subj: "Anglais technique", note: [34]},
-    {id:3.07, subj: "Bases de la communication", note: [34]},
-    {id:3.08, subj: "Introduction à l'rchitecture des ordinateurs", note: [34]},
-    {id:3.09, subj: "Intro. aux syst. d'expl.", note: [34]},
-    {id:3.10, subj: "Mathématiques discrètes", note: [34]},
-    {id:3.11, subj: "Outils mathématiques fondamentaux", note: [34]},
-    {id:3.12, subj: "Comm et fonctionnment bas niveau", note: [34]},
-    {id:3.13, subj: "Graphes", note: [34]},
-    {id:3.14, subj: "Méthodes Numériques", note: [34]},
-    {id:3.15, subj: "Introduction aux services réseaux", note: [34]},
-    {id:3.16, subj: "Introduction aux base de données et SQL", note: [34]},
-    {id:3.17, subj: "Economie durable et numérique", note: [34]},
-    {id:3.18, subj: "Exploitation d'une base de données", note: [34]},
-    {id:3.19, subj: "Outils numériques pour les statistiques desc", note: [34]},
-    {id:3.20, subj: "Gestion de projet et des organisations", note: [34]},
-    {id:3.21, subj: "Projet professionnel et personnel", note: [34]},
-    {id:3.22, subj: "Droit des contrats et du numérique", note: [34]},
+    {id:3.01, subj: "Initiation au développement", note: [], coef31:21, coef32:12},
+    {id:3.02, subj: "Développement d'interfaces web", note: [], coef31:6, coef35:9, coef36: 2.5},
+    {id:3.03, subj: "Développement orienté objets", note: [], coef31:10.5, coef32:7.5},
+    {id:3.04, subj: "Développement d'applications avec IHM", note: [], coef31:10.5, coef35:1.5, coef36: 2},
+    {id:3.05, subj: "Qualité de developpement", note: [], coef31: 6, coef35: 3},
+    {id:3.06, subj: "Anglais technique", note: [], coef31: 3, coef33: 9, coef34: 3, coef35: 3, coef36: 14},
+    {id:3.07, subj: "Bases de la communication", note: [], coef31: 3, coef33: 4.5, coef35: 12, coef36: 11},
+    {id:3.08, subj: "Introduction à l'rchitecture des ordinateurs", note: [], coef32: 1.5, coef33: 10.5},
+    {id:3.09, subj: "Intro. aux syst. d'expl.", note: [], coef32: 1.5, coef33: 10.5},
+    {id:3.10, subj: "Mathématiques discrètes", note: [], coef32: 7.5, coef34: 9},
+    {id:3.11, subj: "Outils mathématiques fondamentaux", note: [], coef32: 7.5},
+    {id:3.12, subj: "Comm et fonctionnment bas niveau", note: [], coef32: 6, coef33: 18},
+    {id:3.13, subj: "Graphes", note: [], coef32: 10.5, coef35: 3},
+    {id:3.14, subj: "Méthodes Numériques", note: [], coef32: 6},
+    {id:3.15, subj: "Introduction aux services réseaux", note: [], coef33: 7.5},
+    {id:3.16, subj: "Introduction aux base de données et SQL", note: [], coef34: 18},
+    {id:3.17, subj: "Economie durable et numérique", note: [], coef34: 3, coef36: 5.5},
+    {id:3.18, subj: "Exploitation d'une base de données", note: [], coef34: 15},
+    {id:3.19, subj: "Outils numériques pour les statistiques desc", note: [], coef34: 6},
+    {id:3.20, subj: "Gestion de projet et des organisations", note: [], coef34: 6, coef35: 28.5, coef36: 5.5},
+    {id:3.21, subj: "Projet professionnel et personnel", note: [], coef36: 11},
+    {id:3.22, subj: "Droit des contrats et du numérique", note: [], coef36: 8.5},
   ]
 
   async function userData(iduser) {
@@ -57,13 +57,14 @@ function Home() {
   }
   
   useEffect(() => {
-    setArrayNotes(Ressources);
+    /* setArrayNotes(Ressources); */
     async function fetchNotes() {
       const notesFetched = await userData(user.email);
       setArrayNotes(notesFetched);
     }
     fetchNotes();
-  }, [])
+  }, []);
+
   if(loading) return <h1>Loading</h1>
   return (
     <div id='divHome'>
--- src/Components/Subjects.js
+++ src/Components/Subjects.js
@@ -8,10 +8,10 @@ const firestore = getFirestore(app);
 
 const Subjects = ({notes, setArrayNotes, email, userData}) => {
     const [valorInput, setValorInput] = useState();
-
+    
     async function handleSuprmir (id) {
         id.note.pop();
-
+        
         const newNote = notes;
         
         console.log(newNote, id);
@@ -25,29 +25,86 @@ const Subjects = ({notes, setArrayNotes, email, userData}) => {
         e.preventDefault();
         console.log(e)
         console.log(e.nativeEvent.path[2].childNodes[0].data);
-
+        
         if(isNaN(valorInput)) {return console.log(null);}
         else {
             let index;
             for (const key in notes) {
-                if(notes[key].id === Number(e.nativeEvent.path[2].childNodes[0].data)) index = key;
+                if(notes[key].id === Number(e.nativeEvent.path[2].childNodes[0].innerHTML)) index = key;
             }
-            
-            notes[index].note.push(Number(valorInput))
+            console.log(notes[index]);
+            notes[index].note.push(Number(valorInput));
             const newNote = notes;
-    
+            
             const docuRef = doc(firestore,`users/${email}`);
             await updateDoc(docuRef, {Ressources: [...newNote]});
             await setArrayNotes(newNote);
         }
         e.nativeEvent.path[0][0].value = '';
-       
+        
     }
     
     async function handleChange(val) {
         val.preventDefault();
         setValorInput(val.target.value);
     }
+    
+    let ue31 = 0;
+    let ue32 = 0;
+    let ue33 = 0;
+    let ue34 = 0;
+    let ue35 = 0;
+    let ue36 = 0;
+    async function calcul() {
+        
+        for (const key in notes) {
+            if (notes[key].coef31) {
+                for (let i = 0; i < notes[key].note.length; i++) {
+                    ue31 += notes[key].coef31 * notes[key].note[i];
+                    
+                }
+            }
+            if (notes[key].coef32) {
+                for (let i = 0; i < notes[key].note.length; i++) {
+                    ue32 += notes[key].coef32 * notes[key].note[i];
+                    
+                }
+            }
+            if (notes[key].coef33) {
+                for (let i = 0; i < notes[key].note.length; i++) {
+                    ue33 += notes[key].coef33 * notes[key].note[i];
+                    
+                }
+            }
+            if (notes[key].coef34) {
+                for (let i = 0; i < notes[key].note.length; i++) {
+                    ue34 += notes[key].coef34 * notes[key].note[i];
+                    
+                }
+            }
+            if (notes[key].coef35) {
+                for (let i = 0; i < notes[key].note.length; i++) {
+                    ue35 += notes[key].coef35 * notes[key].note[i];
+                    
+                }
+            }
+            if (notes[key].coef36) {
+                for (let i = 0; i < notes[key].note.length; i++) {
+                    ue36 += notes[key].coef36 * notes[key].note[i];
+                    
+                }
+            }
+            
+        }
+        ue31 = (ue31/100).toFixed(2);
+        ue32 = (ue32/100).toFixed(2);
+        ue33 = (ue33/100).toFixed(2);
+        ue34 = (ue34/100).toFixed(2);
+        ue35 = (ue35/100).toFixed(2);
+        ue36 = (ue36/100).toFixed(2);
+        
+    }
+    
 
     useEffect(() => {
         async function fetchNotes() {
@@ -55,15 +112,22 @@ const Subjects = ({notes, setArrayNotes, email, userData}) => {
           setArrayNotes(notesFetched);
         }
         fetchNotes();
-        
-    }, [notes]);
+    }, [notes, calcul()]);
 
     return (
         <div id="divSubjects">
+            <div>
+                <div style={{backgroundColor: ue31>=6? 'green': '#b51a1a'}}><p>UE 3.1:</p> {ue31} /12</div>
+                <div style={{backgroundColor: ue32>=6? 'green': '#b51a1a'}}><p>UE 3.2:</p> {ue32} /12</div>
+                <div style={{backgroundColor: ue33>=6? 'green': '#b51a1a'}}><p>UE 3.3:</p> {ue33} /12</div>
+                <div style={{backgroundColor: ue34>=6? 'green': '#b51a1a'}}><p>UE 3.4:</p> {ue34} /12</div>
+                <div style={{backgroundColor: ue35>=6? 'green': '#b51a1a'}}><p>UE 3.5:</p> {ue35} /12</div>
+                <div style={{backgroundColor: ue36>=6? 'green': '#b51a1a'}}><p>UE 3.6:</p> {ue36} /12</div>
+            </div>
             
             {notes.map((subj) => {
                 return (
-                    <span key={subj.id}>
+                    <span>
                         <h2>{subj.id}</h2>
                         <h3> {subj.subj}</h3>
                         
